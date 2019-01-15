f145829728e2eb0aa2190e10682258ef0626869a
joyc <joyc@live.jp>
2019-01-16 00:21:29 +0900

    add category forms

--- hylog/forms.py
+++ hylog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, ValidationError
 from wtforms.validators import DataRequired, Length
 from flask_ckeditor import CKEditorField
 
@@ -25,3 +25,13 @@ class PostForm(FlaskForm):
         super(PostForm, self).__init__(*args, **kwargs)
         self.category.choices = [(category.id, category.name)
                                  for category in Category.query.order_by(category.name).all()]
+
+
+class CategoryForm(FlaskForm):
+    """分类目录表单"""
+    name = StringField('分类名', validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField()
+
+    def validate_name(self, field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError('该分类名已被使用，请换一个试试。')
