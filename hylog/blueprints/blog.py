b4c2eef6a40556d66f8913c90250fddaab501fc6
chuang.li <chuang.li@accenture.com>
2019-01-16 12:56:32 +0900

    add blog blueprint view

--- hylog/blueprints/blog.py
+++ hylog/blueprints/blog.py
@@ -1,13 +1,23 @@
-from flask import Blueprint
+from flask import render_template, Blueprint
 
 blog_bp = Blueprint('blog', __name__)
 
 
+@blog_bp.route('/')
+def index():
+    return render_template('blog/index.html')
+
+
 @blog_bp.route('/about')
 def about():
-    return '关于页面'
+    return render_template('blog/about.html')
 
 
 @blog_bp.route('/category/<int:category_id>')
-def category(category_id):
-    return '目录页面'
+def show_category(category_id):
+    return render_template('blog/category.html')
+
+
+@blog_bp.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    return render_template('blog/post.html')
